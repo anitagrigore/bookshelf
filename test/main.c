d9f52a1d3677010fd74b7336172d273328e0601b
Victor Barbu <victorbarbu08@gmail.com>
2019-09-03 20:52:18 +0300

    Fix lost memory in test runner

--- test/main.c
+++ test/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "cutest/CuTest.h"
 
@@ -10,11 +11,29 @@ int main()
   CuString *output = CuStringNew();
   CuSuite *all = CuSuiteNew();
 
-  CuSuiteAddSuite(all, make_suite_list());
-  CuSuiteAddSuite(all, make_suite_hashtable());
+  CuSuite *suites[] = {
+    make_suite_list(),
+    make_suite_hashtable(),
+  };
+
+  const int32_t suites_count = sizeof(suites) / sizeof(CuSuite *);
+
+  int32_t suite_idx;
+  for (suite_idx = 0; suite_idx < suites_count; suite_idx++)
+  {
+    CuSuiteAddSuite(all, suites[suite_idx]);
+  }
 
   CuSuiteRun(all);
   CuSuiteSummary(all, output);
   CuSuiteDetails(all, output);
   printf("%s\n", output->buffer);
+
+  for (suite_idx = 0; suite_idx < suites_count; suite_idx++)
+  {
+    CuSuiteDelete(suites[suite_idx]);
+  }
+
+  CuStringDelete(output);
+  free(all);
 }
