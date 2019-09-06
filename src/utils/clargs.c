99f733fc4088cd345dac7c7c327fb397441359ce
Anita Grigore <anita.grigore1@gmail.com>
2019-09-06 14:37:35 +0300

    [WIP] Implement arguments parser

--- src/utils/clargs.c
+++ src/utils/clargs.c
@@ -69,3 +69,91 @@ int32_t clargs_parse_int(const char *value, void *extra, char *error)
 
   return atoi(value);
 }
+
+int64_t clargs_parse_long(const char *value, void *extra, char *error)
+{
+  error = NULL;
+
+  if (strlen(value) == 0)
+  {
+    strcpy(error, "failed to parse long integer");
+    return 0;
+  }
+
+  if (strlen(value) == 1 && (value[0] == '+' || value[0] == '-'))
+  {
+    strcpy(error, "failed to parse long integer");
+    return 0;
+  }
+
+  if (value[0] != '+' && value[0] != '-' && !isdigit(value[0]))
+  {
+    strcpy(error, "failed to parse long integer");
+    return 0;
+  }
+
+  int32_t i;
+  for (i = 1; i < strlen(value); i++)
+  {
+    if (!isdigit(value[i]))
+    {
+      strcpy(error, "failed to parse long integer");
+      return 0;
+    }
+  }
+
+  return atoll(value);
+}
+
+int32_t clargs_parse_bool(const char *value, void *extra, char *error)
+{
+  error = NULL;
+  char *allowed = {'1', '0', 't', 'f', 'y', 'n'};
+
+  if (strlen(value) == 0)
+  {
+    return 1;
+  }
+
+  if (strlen(value) == 1)
+  {
+    int32_t i, found = 0;
+    for (i = 0; i < 6; i++)
+    {
+      if (value[0] == allowed[i])
+      {
+        found = 1;
+        return 1;
+      }
+    }
+
+    if (found == 0)
+    {
+      strcpy(error, "failed to parse bool");
+      return 0;
+    }
+
+    return 1;
+  }
+
+  if (!strcmp(value, "true") && !strcmp(value, "false") && !strcmp(value, "yes") && !strcmp(value, "no"))
+  {
+    strcpy(error, "failed to parse bool");
+    return 0;
+  }
+
+  return 1;
+}
+
+char *clargs_parse_string(const char *value, void *extra, char *error)
+{
+  error = NULL;
+
+  if (strlen(value) > (int32_t)extra)
+  {
+    strcpy(error, "failed to parse string");
+    return 0;
+  }
+
+  return value;
+}
