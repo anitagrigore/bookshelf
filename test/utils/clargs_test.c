983399a6e82dd647ae9a3621ad6483e73d5a9d59
Victor Barbu <victorbarbu08@gmail.com>
2020-02-01 12:36:01 +0200

    Add unit test for clargs_parse_bool

--- test/utils/clargs_test.c
+++ test/utils/clargs_test.c
@@ -10,7 +10,7 @@ void test_clargs_parse(CuTest *tc)
 
 }
 
-struct __fixture_parse_int
+struct __fixture_parse_numeric_like
 {
   char raw[24];
   int32_t has_error;
@@ -19,7 +19,7 @@ struct __fixture_parse_int
 
 void test_clargs_parse_int(CuTest *tc)
 {
-  struct __fixture_parse_int test_cases[] = {
+  struct __fixture_parse_numeric_like test_cases[] = {
     {"16", 0, 16},
     {"-10", 0, -10},
     {"+10", 0, 10},
@@ -30,7 +30,7 @@ void test_clargs_parse_int(CuTest *tc)
     {"9999999999999999999", 1, 0},  // out of range
   };
 
-  int32_t n = sizeof(test_cases) / sizeof(struct __fixture_parse_int);
+  int32_t n = sizeof(test_cases) / sizeof(struct __fixture_parse_numeric_like);
   char trace_msg[32] = {0};
 
   int32_t i;
@@ -52,7 +52,44 @@ void test_clargs_parse_int(CuTest *tc)
 
 void test_clargs_parse_bool(CuTest *tc)
 {
+  struct __fixture_parse_numeric_like test_cases[] = {
+    {"true", 0, 1},
+    {"True", 0, 1},
+    {"t", 0, 1},
+    {"T", 0, 1},
+    {"", 0, 1},
+    {"yes", 0, 1},
+    {"y", 0, 1},
+    {"1", 0, 1},
+    {"false", 0, 0},
+    {"fALSE", 0, 0},
+    {"False", 0, 0},
+    {"f", 0, 0},
+    {"no", 0, 0},
+    {"n", 0, 0},
+    {"0", 0, 0},
+    {" false ", 1, 0},
+    {" true ", 1, 0},
+  };
 
+  int32_t n = sizeof(test_cases) / sizeof(struct __fixture_parse_numeric_like);
+  char trace_msg[32] = {0};
+
+  int32_t i;
+  for (i = 0; i < n; i++)
+  {
+    snprintf(trace_msg, sizeof(trace_msg), "test case #%d", i);
+
+    int32_t has_error = 0;
+    char error[64] = {0};
+    int32_t actual = clargs_parse_bool(test_cases[i].raw, NULL, &has_error, error);
+
+    CuAssertIntEquals_Msg(tc, trace_msg, test_cases[i].has_error, has_error);
+    if (!test_cases[i].has_error)
+    {
+      CuAssertIntEquals_Msg(tc, trace_msg, test_cases[i].expected, actual);
+    }
+  }
 }
 
 void test_clargs_parse_long(CuTest *tc)
